12dc9375ea52bad241318062411f622abd57c549
Dev-Researcher <174758270+dev-researcher@users.noreply.github.com>
2024-08-26 16:59:04 -0600

    conditions

--- cars.c
+++ cars.c
@@ -1,88 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
+#include <stdint.h>
 
-#define EAST 0
-#define WEST 1
+#define EAST 1
+#define WEST 2
+#define MAX_CARS 10
 
 int east_count = 0;
 int west_count = 0;
-pthread_mutex_t bridge_mutex;
-pthread_cond_t bridge_cond;
 
-void *car(void *direction_ptr)
-{
-    int direction = (int)direction_ptr;
-    pthread_mutex_lock(&bridge_mutex);
-    if (direction == EAST)
-    {
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+
+#define DEBUG 1
+
+void *car(void *direction_ptr) {
+    int direction = (intptr_t)direction_ptr;
+
+    if (DEBUG) {
+        printf("Carro llegó desde %s\n", direction == EAST ? "Este" : "Oeste");
+    }
+
+    pthread_mutex_lock(&mutex);
+    if (direction == EAST) {
         east_count++;
-        while (west_count > 0 || (east_count > 1))
-        {
-            pthread_cond_wait(&bridge_cond, &bridge_mutex);
+        while (west_count > 0 || east_count > 1) {
+            if (DEBUG) {
+                printf("Carro esperando para cruzar de Este a Oeste\n");
+            }
+            pthread_cond_wait(&cond, &mutex);
         }
-    }
-    else
-    {
+    } else {
         west_count++;
-        while (east_count > 0 || (west_count > 1))
-        {
-            pthread_cond_wait(&bridge_cond, &bridge_mutex);
+        while (east_count > 0 || west_count > 1) {
+            if (DEBUG) {
+                printf("Carro esperando para cruzar de Oeste a Este\n");
+            }
+            pthread_cond_wait(&cond, &mutex);
         }
     }
-    printf("Carro cruzando de %s\n", direction == EAST ? "Este a Oeste" : "Oeste a Este");
-    sleep(1);
-    if (direction == EAST)
-    {
-        east_count--;
+
+    if (DEBUG) {
+        printf("Carro cruzó de %s\n", direction == EAST ? "Este a Oeste" : "Oeste a Este");
     }
-    else
-    {
+
+    if (direction == EAST) {
+        east_count--;
+    } else {
         west_count--;
     }
-    pthread_cond_broadcast(&bridge_cond);
-    pthread_mutex_unlock(&bridge_mutex);
-    pthread_exit(NULL);
+
+    pthread_cond_broadcast(&cond);
+    pthread_mutex_unlock(&mutex);
+
+    return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        printf("Uso: %s <número de autos de Este> <número de autos de Oeste>\n", argv[0]);
-        return -1;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Uso: %s <carros_este> <carros_oeste>\n", argv[0]);
+        return 1;
     }
 
-    int east_cars = atoi(argv[1]);
-    int west_cars = atoi(argv[2]);
+    int cars_east = atoi(argv[1]);
+    int cars_west = atoi(argv[2]);
 
-    pthread_t threads[east_cars + west_cars];
-    int directions[east_cars + west_cars];
+    pthread_t cars[MAX_CARS];
 
-    pthread_mutex_init(&bridge_mutex, NULL);
-    pthread_cond_init(&bridge_cond, NULL);
-
-    for (int i = 0; i < east_cars; i++)
-    {
-        directions[i] = EAST;
-        pthread_create(&threads[i], NULL, car, &directions[i]);
+    for (int i = 0; i < cars_east; i++) {
+        pthread_create(&cars[i], NULL, car, (void *)(intptr_t)EAST);
     }
 
-    for (int i = 0; i < west_cars; i++)
-    {
-        directions[east_cars + i] = WEST;
-        pthread_create(&threads[east_cars + i], NULL, car, &directions[east_cars + i]);
+    for (int i = cars_east; i < cars_east + cars_west; i++) {
+        pthread_create(&cars[i], NULL, car, (void *)(intptr_t)WEST);
     }
 
-    for (int i = 0; i < east_cars + west_cars; i++)
-    {
-        pthread_join(threads[i], NULL);
+    for (int i = 0; i < cars_east + cars_west; i++) {
+        pthread_join(cars[i], NULL);
     }
-
-    pthread_mutex_destroy(&bridge_mutex);
-    pthread_cond_destroy(&bridge_cond);
 
     return 0;
 }
